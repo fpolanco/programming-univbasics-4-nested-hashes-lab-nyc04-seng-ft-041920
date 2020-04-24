3c7d8afd41706a200b01e574ae017a98b2c9ba65
Franchell Polanco <polanco.franchell@gmail.com>
2020-04-24 17:22:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-    programmer_hash[:alan_kay][:known_for]
+    programmer_hash[:alan_kay][:known_for]["Object Orientation"]
 end
    #programmer_hash[:alan_kay][:known_for][0]
 #end
